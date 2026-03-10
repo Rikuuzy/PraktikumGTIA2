0ca064d4ab11603040717fd0a272618f23c43cfd
Hadrian Shandhy Yudha <shandhyyudha@gmail.com>
2026-03-10 08:50:57 +0700

    Basic Geometric Shape | OpenGL Primitives

--- Pertemuan 1/GL Quad Strip/main.cpp
+++ Pertemuan 1/GL Quad Strip/main.cpp
@@ -10,6 +10,9 @@ void QuadStrip(void) {
     glVertex3f(0.0f,  0.5f, 0.0f);
     glVertex3f(-0.5f,  0.0f, 0.0f);
     glVertex3f(-0.5f,  0.5f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f,  0.0f, 0.0f);
+	glVertex3f(-1.0f,  0.5f, 0.0f);
 	glEnd();
 	glFlush();
 }
